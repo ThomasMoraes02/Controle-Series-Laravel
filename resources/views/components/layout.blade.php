524a04541b0b5331782f3d9546513079bab5857e
Thomas Moraes <tho.vini7@gmail.com>
2023-05-12 19:55:14 -0300

    Lapidando Aplicacao - Controllers, Routes, Views

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>{{ $title }}</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css'
+        integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA=='
+        crossorigin='anonymous' />
+</head>
+
+<body>
+    <div class="container">
+        <h1>{{ $title }}</h1>
+        {{ $slot }}
+    </div>
+    <script src="" async defer></script>
+</body>
+
+</html>
