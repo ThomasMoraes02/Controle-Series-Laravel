068efff2e9907845ebcdb2b9ea2739cc95ea331c
Thomas Moraes <tho.vini7@gmail.com>
2023-05-16 18:52:25 -0300

    Marcando episodios como assistidos

--- app/Http/Controllers/EpisodesController.php
+++ app/Http/Controllers/EpisodesController.php
@@ -1,7 +1,9 @@
 <?php 
 namespace App\Http\Controllers;
 
+use App\Models\Episode;
 use App\Models\Season;
+use Illuminate\Http\Request;
 
 class EpisodesController
 {
@@ -10,5 +12,23 @@ class EpisodesController
         return view('episodes.index', [
             'episodes' => $season->episodes
         ]);
+    }
+
+     /**
+     * Updates the watched status of episodes of a season based on the input request.
+     *
+     * @param Request $request the HTTP request object containing the list of watched episode IDs
+     * @param Season $season the season object whose episodes are to be updated
+     */
+    public function update(Request $request, Season $season)
+    {
+        $watchedEpisodes = $request->episodes;
+        $season->episodes->each(function(Episode $episode) use($watchedEpisodes) {
+            $episode->watched = in_array($episode->id, $watchedEpisodes);
+        });
+
+        $season->push();
+
+        return to_route('episodes.index', $season->id);
     }
 }
--- resources/views/episodes/index.blade.php
+++ resources/views/episodes/index.blade.php
@@ -6,8 +6,7 @@
             @foreach ($episodes as $episode)
                 <li class="list-group-item d-flex justify-content-between align-items-center">
                     Episódio: {{ $episode->number }}
-
-                    <input type="checkbox" name="episodes[]" value="{{ $episode->id }}">
+                    <input type="checkbox" name="episodes[]" value="{{ $episode->id }}" @if ($episode->watched) checked @endif>
                 </li>
             @endforeach
         </ul>
